b9358cce30c1ca1efcce4a76ad2b53a4dc9551b4
GiriTejaBille <giriteja94495@gmail.com>
2020-08-14 12:42:11 +0530

    Create Minimum Size Subarray Sum.cpp

---
+++ Sliding-Window/Minimum Size Subarray Sum.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/minimum-size-subarray-sum/
+
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int begin=0;
+        int end=0;
+        int n=nums.size();
+        if(num)
+        int len=0;
+        int temp=0;
+        while(end< n){
+            
+            if(nums[i]>=s) return 1;
+            elseif (nums[i])
+            else{
+                temp+=nums[i];
+            }
+            
+            
+        }
+    }
+};
