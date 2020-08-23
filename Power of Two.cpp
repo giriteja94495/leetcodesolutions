2ebc0e12a9e98bbc7c27e00650a8f1c6fffb8244
GiriTejaBille <giriteja94495@gmail.com>
2020-08-23 22:25:15 +0530

    Update Power of Two.cpp

--- Power of Two.cpp
+++ Power of Two.cpp
@@ -12,3 +12,16 @@ public:
         
     }
 };
+
+// this previous solution is so stupid ...it didn't strike me to use the concept of set bit count
+// THIS CAN BE EASILY SOLVED USING Brian Kernighan’s Algorithm:
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+       if(n==0) return false; 
+        return !(n&(n-1));
+    }
+};
+
+// the above can be simplified further into 
+// if n is 0 ,we return false ....am i right? so lets combine both the statements as return (n & !(n & n-1));
