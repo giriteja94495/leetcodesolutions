16c7c8dc8d06481412ef8000c55e812154edf665
GiriTejaBille <giriteja94495@gmail.com>
2020-08-24 15:55:44 +0530

    Update Factorial Trailing Zeroes.cpp

--- Basic/Factorial Trailing Zeroes.cpp
+++ Basic/Factorial Trailing Zeroes.cpp
@@ -21,3 +21,14 @@ public:
         return count;
     }
 };
+// I felt my above solution is the best .....but it isn't..it can be still optimised further ...see this below one
+
+class Solution {
+public:
+      int  count=0;
+    int trailingZeroes(int n) {
+        if(n==0) return 0;
+        count=(n/5 + trailingZeroes(n/5));
+        return count; // if you hate using global variable ..you can use 1 line code such as return n==0 ? 0 : (n/5+trailingZeroes(n/5));
+    }
+};
