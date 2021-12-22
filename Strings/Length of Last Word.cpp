acca2061270776ecfc6747a5d5b26b7d4965d8da
GiriTejaBille <giriteja94495@gmail.com>
2021-12-22 20:10:50 +0530

    Update Length of Last Word.cpp

--- Strings/Length of Last Word.cpp
+++ Strings/Length of Last Word.cpp
@@ -19,3 +19,24 @@ public:
 
 
 // https://leetcode.com/problems/length-of-last-word/
+
+
+// the solution i loved
+
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+		
+		// We are looking for the last word so let's go backward
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') { // a letter is found so count
+                length++;
+            } else {  // it's a white space instead
+				//  Did we already started to count a word ? Yes so we found the last word
+                if (length > 0) return length;
+            }
+        }
+        return length;
+    }
+}
