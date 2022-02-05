c588b767fb2311a0bf1bf76544fb769cb274d928
GiriTejaBille <giriteja94495@gmail.com>
2022-02-05 12:04:58 +0530

    Create Substrings of Size Three with Distinct Characters.cpp

---
+++ Sliding-Window/Substrings of Size Three with Distinct Characters.cpp
@@ -0,0 +1,31 @@
+// Brute Force solution 
+
+class Solution {
+public:
+    bool isGood(string str){
+        unordered_map<char,int> mp;
+        for(char a:str){
+        mp[a]++;    
+        }
+        for(auto itr=mp.begin();itr!=mp.end();itr++){
+            cout<<itr->second<<str<<endl;
+            if(itr->second>1) return false;
+        }
+        return true;
+    }
+    int countGoodSubstrings(string s) {
+        if(s.size()<3){
+            return 0;
+        }
+        int count=0;
+        for(int i=0;i<=s.size()-3;i++)
+        {
+            string str=s.substr(i,3);
+            if(isGood(str)){
+                count++;
+            }
+            
+        }
+        return count;
+    }
+};
