ce7a250303762f14b90aff624328e486cf16c1d2
GiriTejaBille <giriteja94495@gmail.com>
2022-02-15 11:01:16 +0530

    Time: 36 ms (31.33%), Space: 20 MB (14.62%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int res=0;
-       for(auto a:nums){
-           res^=a;
-       } 
-        return res;
+       // int res=0;
+       // for(auto a:nums){
+       //     res^=a;
+       // } 
+       //  return res;
+        
+        unordered_map<int,int> mp ;
+        for(auto a:nums) mp[a]++;
+        int ans=0;
+        for(auto itr=mp.begin();itr!=mp.end();itr++) {
+        if(itr->second==1) return itr->first;
+        }
+        return 0;
+        
+       
     }
 };
