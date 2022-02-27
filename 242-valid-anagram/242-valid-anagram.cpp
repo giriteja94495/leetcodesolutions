c4d4373c789bb7c668abac94d2468eefe8387730
GiriTejaBille <giriteja94495@gmail.com>
2022-02-27 10:43:12 +0530

    Time: 25 ms (13.45%), Space: 7.2 MB (78.43%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        //sorting solution
+        
+       sort(s.begin(),s.end());
+       sort(t.begin(),t.end());
+       return s==t;
+        
+        
+        
+        
+        // easy solution but hard to think at the start
+        
         vector<int> all_char(26,0);
         for(auto a:s){
             all_char[a-'a']++;
