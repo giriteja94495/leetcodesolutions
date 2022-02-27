e8887643eef7ecd8939fd3f2ec73bd56eb969928
GiriTejaBille <giriteja94495@gmail.com>
2022-02-27 10:29:01 +0530

    Time: 174 ms (22.74%), Space: 53.6 MB (19.70%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -23,6 +23,10 @@ public:
         
         // sorting solution 
         
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++) if(nums[i]==nums[i+1]) return true;
+        return false;
+        
         
     }
     
