fbaf16c431904a87e77a0d0ddc2989ecdc43ae99
GiriTejaBille <giriteja94495@gmail.com>
2022-02-27 13:34:53 +0530

    Time: 108 ms (83.76%), Space: 67.6 MB (91.19%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        
+        //kaden's algorithm
         int maxS=INT_MIN;
         int local=0;
         for(int i=0;i<nums.size();i++){
@@ -8,6 +10,21 @@ public:
             maxS= max(local,maxS);
         }
         return maxS;
+        
+        
+        // dp solution 
+        
+        
+        vector<int> dp(nums.size());
+        dp[0]=nums[0];
+        int ans=INT_MIN;
+        for(int i=1;i<nums.size();i++){
+            dp[i]=max(dp[i],dp[i-1]+nums[i]);
+            ans=max(ans,dp[i]);
+        }
+        return ans;
+        
+        
     }
     
 };
