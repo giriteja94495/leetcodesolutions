78b3fced89bc7589fe10e73e86ed7e1d4b690e96
GiriTejaBille <giriteja94495@gmail.com>
2022-03-02 20:52:02 +0530

    Time: 6 ms (23.26%), Space: 6.3 MB (90.23%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -4,18 +4,29 @@ public:
     bool helper(string a, string b, int m , int n ){
         if(m==0) return true;
         if(n==0) return false;
-        
         if(a[m-1]==b[n-1]) {
           return helper(a,b,m-1,n-1);
         }
        return helper(a,b,m,n-1);
-        
-      
+    
     }
     
     
     bool isSubsequence(string s, string t) {
-     
-        return helper(s,t,s.size(),t.size());
+        
+        int idx1=0,idx2=0;
+        while(idx1<s.size() && idx2<t.size()){
+            if(s[idx1]==t[idx2]) {
+                idx1++;
+                idx2++;
+            }
+            else{
+                idx2++;
+            }
+        }
+        return idx1==s.size();
+        
+        
+        // return helper(s,t,s.size(),t.size());
     }
 };
