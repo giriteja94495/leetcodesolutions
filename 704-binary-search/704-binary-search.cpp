19128d1c1c1a3b999beef4155d9219a5539824cf
GiriTejaBille <giriteja94495@gmail.com>
2022-06-25 20:44:10 +0530

    Time: 71 ms (19.22%), Space: 27.6 MB (12.57%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
+    int search(vector<int>& nums, int target) {
         int left=0;
-        int right=arr.size()-1;
-        int mid;
+        int right=nums.size()-1;
         while(left<=right){
-             mid=left+(right-left)/2;
-            if(arr[mid]==target) return mid;
-            else if(arr[mid]<target) left=mid+1;
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) left=mid+1;
             else right=mid-1;
-           
         }
         return -1;
+        
     }
 };
