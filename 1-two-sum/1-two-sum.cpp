a1214c4e35d65e7ac1ba7f2898671fafd2144721
GiriTejaBille <giriteja94495@gmail.com>
2022-06-30 19:35:26 +0530

    Time: 38 ms (40.75%), Space: 11.9 MB (15.49%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,73 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        // basic intution 
-        // Time Complexity - O(n^2)
-        // Space comeplexity - constant space O(1)
-//         int a =0;
-//         int b=0;
-        
-//         for(int i=0;i<nums.size()-1;i++){
-//             for(int j=i+1;j<nums.size();j++){
-//                 if(nums[i]+nums[j]==target){
-//                     a=i;
-//                     b=j;
-//                     break;
-//                 }
-//             }
-//         }
-//         return {a,b};
-        
-        // Binary search approach 
-        
-        
-//         vector<pair<int,int>> v;
-//         for(int i=0;i<nums.size();i++){
-//             v.push_back(make_pair(nums[i],i));
-//         }
-//         sort(v.begin(),v.end());
-        
-//         int i=0;
-//         int j=nums.size()-1;
-//         int a=0,b=0;
-        
-//         while(i<j){
-//             if(v[i].first + v[j].first == target){
-//                 a=v[i].second;
-//                 b=v[j].second;
-//                 break;
-//             }
-//             else if(v[i].first + v[j].first < target) i++;
-//             else j--;
-//         }
-//         return {a,b};
-        
     
-        //map based approach
+        // unordered_map<int,int>
         
         unordered_map<int,int> mp;
-        int a=0,b=0;
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]=i;
         }
-          for(int i=0;i<nums.size();i++){
-            int val=target-nums[i];
-              if(mp.find(val)!=mp.end()  ){
-                  if(mp[val]!=i){
-                        a=i;
-                  b=mp[val];
-                  break;
-                 }  
-              }
+        for(int i=0;i<nums.size();i++){
+            int val = target-nums[i];
+            if(mp.find(val)!=mp.end()){
+                if(i!=mp[val]){
+                    return {i,mp[val]};
+                }
+            }
         }
-        return {a,b};
-      
-        
-    
-        
-     
-
+        return {0,0};
         
         
     }
