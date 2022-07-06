b45366effa5f368c8d5f38e04aed0d7003c221d6
GiriTejaBille <giriteja94495@gmail.com>
2022-07-06 08:58:22 +0530

    Time: 17 ms (28.08%), Space: 5.8 MB (80.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     int fib(int n) {
+        // recursion model - 0(2^n)- time complexity
         if(n==0 || n==1) return n;
         
         return fib(n-1)+fib(n-2);
+        
+        
+        // dp model 
+        
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+        
     }
 };
