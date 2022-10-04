a357c46bd1ab93f45a0efddcb152eaf04259f162
GiriTejaBille <giriteja94495@gmail.com>
2022-10-04 13:59:14 +0530

    Time: 2 ms (62.20%), Space: 6.3 MB (72.21%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -23,14 +23,15 @@ public:
     bool backspaceCompare(string s, string t) {
         string s1= formString(s);
         string s2 = formString(t);
-        cout<<s1<<endl;
-        cout<<s2<<endl;
-        if(s1.size() != s2.size()) return false;
-        else{
-            for(int i=0;i<s1.size() && i<s2.size();i++){
-                if(s1[i]!= s2[i]) return false;
-            }
-            return true;
-        }
+        // cout<<s1<<endl;
+        // cout<<s2<<endl;
+        // if(s1.size() != s2.size()) return false;
+        // else{
+        //     for(int i=0;i<s1.size() && i<s2.size();i++){
+        //         if(s1[i]!= s2[i]) return false;
+        //     }
+        //     return true;
+        // }
+        return s1==s2;
     }
 };
