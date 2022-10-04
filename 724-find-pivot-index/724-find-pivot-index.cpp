d4d6de4e6b28ea53084f31847887bd0e6303c7fc
GiriTejaBille <giriteja94495@gmail.com>
2022-10-04 18:13:32 +0530

    Time: 51 ms (39.67%), Space: 31.2 MB (51.90%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int n = nums.size();
-        // create 2 arrays and calculate the sum on each parts from left and right 
-        vector<int> leftSum(n,0);
-        vector<int> rightSum(n,0);
+//         int n = nums.size();
+//         // create 2 arrays and calculate the sum on each parts from left and right 
+//         vector<int> leftSum(n,0);
+//         vector<int> rightSum(n,0);
         
-        for(int i=1;i<n;i++){
-            leftSum[i] = (nums[i-1]+leftSum[i-1]);
+//         for(int i=1;i<n;i++){
+//             leftSum[i] = (nums[i-1]+leftSum[i-1]);
+//         }
+//         for(int j=n-2; j>=0;j--){
+//             rightSum[j] =(nums[j+1]+rightSum[j+1]);
+//             cout<<rightSum[j];
+//         }
+//         // loop through the both arrays and find out at which index leftSum == rightSum
+//         for(int i=0;i<n;i++){
+//             if(leftSum[i]==rightSum[i]) return i;
+//         }
+//         // if we didn't find any, then return -1
+//         return -1;
+        
+        
+        int sumFromLeft =0;
+        int sum=0;
+        for(auto a:nums) sum+=a;
+        
+        for(int i=0;i<nums.size();i++){
+            if(sumFromLeft == sum - nums[i] -sumFromLeft) return i ;
+            sumFromLeft+=nums[i];
         }
-        for(int j=n-2; j>=0;j--){
-            rightSum[j] =(nums[j+1]+rightSum[j+1]);
-            cout<<rightSum[j];
-        }
-        // loop through the both arrays and find out at which index leftSum == rightSum
-        for(int i=0;i<n;i++){
-            if(leftSum[i]==rightSum[i]) return i;
-        }
-        // if we didn't find any, then return -1
         return -1;
     }
 };
