5a75347715ca3f42d61c0df5cf4f446cd7643bb5
GiriTejaBille <giriteja94495@gmail.com>
2022-10-05 11:58:34 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.09%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -4,16 +4,17 @@ public:
         vector<vector<int>> ans;
         ans.push_back({1});
         int k=0;
-        numRows--;
-        while(numRows--){
-            vector<int> temp;
-             temp.push_back(1);
-             for(int i=1;i<ans[k].size();i++){
-                 temp.push_back(ans[k][i] + ans[k][i-1]);
-             }
+        numRows = numRows-1;
+        while(numRows){
+            vector<int> temp ;
             temp.push_back(1);
-            ans.push_back(temp);
+            for(int i =1;i<ans[k].size();i++){
+                temp.push_back(ans[k][i] +ans[k][i-1]);
+            }
+            temp.push_back(1);
             k++;
+            ans.push_back(temp);
+            numRows = numRows-1;
         }
         return ans;
     }
