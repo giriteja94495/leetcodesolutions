58677c1b2ed967187e7f990d15e15e70a6a59562
GiriTejaBille <giriteja94495@gmail.com>
2022-11-03 08:40:12 +0530

    Time: 400 ms (92.06%), Space: 167.9 MB (60.00%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -22,32 +22,5 @@ public:
                 }
         }
         return count;
-//          unordered_map<string,int> avail;
-//         int ans=0;
-//         for(auto &word:words)
-//         {
-//             string rev=word;
-//             reverse(rev.begin(),rev.end());
-//             if(avail[rev]>0)
-//             {
-//                 ans+=4;
-//                 avail[rev]--;
-//             }
-//             else
-//             avail[word]++;
-//         }
-//         for(auto &pr:avail)
-//         {
-//             string word=pr.first;
-//             int cnt=pr.second;
-//             if(word[0]==word[1]&&cnt>0)
-//             {
-//                 ans+=2;
-//                 break;
-//             }
-//         }
-//         return ans;
-        
-        
     }
 };
