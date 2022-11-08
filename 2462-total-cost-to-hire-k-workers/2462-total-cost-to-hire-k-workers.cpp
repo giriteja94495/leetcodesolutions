d3b43a413e10fa0ac51bc5cbd2d129db22401119
GiriTejaBille <giriteja94495@gmail.com>
2022-11-08 20:35:06 +0530

    Time: 434 ms (46.52%), Space: 72.9 MB (64.34%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
         priority_queue<int,vector<int>,greater<int>> p1, p2;
-        long size = costs.size();
-        long maxPossible =100002;
-        long i =0;
-        long j = size-1;
+        int maxPossible = 100001;
+        int i =0;
+        int j = size(costs)-1;
         long long  ans =0;
         while(k--){
             while(p1.size()<candidates && i<=j) p1.push(costs[i++]);
             while(p2.size()<candidates && i<=j) p2.push(costs[j--]);
-
             int a = p1.size() ? p1.top() : maxPossible;
             int b = p2.size() ? p2.top() : maxPossible;
-            
             ans += (a<=b?a:b);
-            
             a<=b ? p1.pop(): p2.pop();
-        
         }
         return ans;
     }
